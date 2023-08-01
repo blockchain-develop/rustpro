426d1cecf5abbd4d48f5d1de6e7675e1f1eb8ced
egaotan <chain_dev@163.com>
2023-08-01 22:13:03 +0800

    update

--- lab/freeze/src/main.rs
+++ lab/freeze/src/main.rs
@@ -2,7 +2,11 @@ fn main() {
     let mut _mutable_integer = 7i32;
     {
         let _mutable_integer = _mutable_integer;
+        let _mutable_integer = 8i32;
+        println!("inner {}", _mutable_integer)
         //_mutable_integer = 50;
     }
+    println!("outer {}", _mutable_integer);
     _mutable_integer = 3;
+    println!("outer {}", _mutable_integer);
 }
