8f965c83b62d214588ef0ede77d21f6ea9dcc085
egaotan <chain_dev@163.com>
2023-08-03 14:59:31 +0800

    update

---
+++ lab/function_example/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    fn1();
+}
+
+fn fn1() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+    //
+    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+    println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+
+    //
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+    println!("2 in array1: {}", array1.iter().any(|&x| x == 2));
+    println!("2 in array2: {}", array2.into_iter().any(|x| x == 2));
+}
+
+fn fn2() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+    //
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
+    //
+    println!("find 2 in vec1: {:?}", iter.find(|&&x| x == 2));
+    println!("find 2 in vec2: {:?}", into_iter.find(|&x| x == 2));
+
+    //
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+    println!("find 2 in array1: {:?}", array1.iter().find(|&&x| x == 2));
+    println!("find 2 in array2: {:?}", array2.into_iter().find(|&x| x == 2));
+}
