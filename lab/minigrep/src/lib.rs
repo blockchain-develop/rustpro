880e34c84d68132d1ce0e1c2ecce3e8be459c3af
egaotan <chain_dev@163.com>
2023-08-17 22:55:17 +0800

    updat

--- lab/minigrep/src/lib.rs
+++ lab/minigrep/src/lib.rs
@@ -54,8 +54,8 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
     */
     contents.lines()
-            .filter(|line| line.contains(query))
-            .collect()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
